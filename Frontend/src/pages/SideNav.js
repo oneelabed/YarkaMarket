494cd00aefc8e311596f534fe03fc7e314d26e1b
oneelabed <oneelabed123@gmail.com>
2025-09-21 15:01:04 +0300

    mobile style adjustments

--- Frontend/src/pages/SideNav.js
+++ Frontend/src/pages/SideNav.js
@@ -13,32 +13,32 @@ function SideNav() {
       <nav className="sideNav">
         <Link to="/"><img alt="logo" src={blackLogo} id="blackLogo"></img><br/></Link>
         <div className="options">
-          <User id="icon" color="black" size={40} style={{marginLeft:"7px"}}></User><b className="sidenav-word" id="name"> {currentUser && currentUser.username}</b><br/><br/><br/><br/>
+          <User className="icon" color="black" size={40} style={{marginLeft:"7px"}}></User><b className="sidenav-word" id="name"> {currentUser && currentUser.username}</b><br/><br/><br/><br/>
           <div className="sidenav-item">
             <NavLink to="market" 
               className={({ isActive }) => `${ isActive ? "active-sidenav" : "unactive-sidenav"}`}>
-            <Home id="icon" color="green" size={20}></Home>
+            <Home className="icon" color="green" size={20}></Home>
             <span className="sidenav-word">Marketplace&nbsp;&nbsp;&nbsp;</span>
             </NavLink>
           </div><br/>
           <div className="sidenav-item">
             <NavLink to="conversations" 
               className={({ isActive }) => `${ isActive ? "active-sidenav" : "unactive-sidenav"}`}>
-            <MessageCircle id="icon" color="blue" size={20}></MessageCircle>
+            <MessageCircle className="icon" color="blue" size={20}></MessageCircle>
             <span className="sidenav-word">Messages&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span>
             </NavLink>
           </div><br/>
           <div className="sidenav-item">
             <NavLink to="create-listing" 
             className={({ isActive }) => `${ isActive ? "active-sidenav" : "unactive-sidenav"}`}>
-            <Plus id="icon" color="purple" size={20}></Plus>
+            <Plus className="icon" color="purple" size={20}></Plus>
             <span className="sidenav-word">Create Listing</span>
             </NavLink>
           </div><br/>
           <div className="sidenav-item">
             <NavLink to="my-listings" 
               className={({ isActive }) => `${ isActive ? "active-sidenav" : "unactive-sidenav"}`}>
-            <ShoppingBag id="icon" color="orange" size={20}></ShoppingBag>
+            <ShoppingBag className="icon" color="orange" size={20}></ShoppingBag>
             <span className="sidenav-word">My Listings&nbsp;&nbsp;&nbsp;&nbsp;</span>
             </NavLink>
           </div><br/>          
@@ -48,7 +48,7 @@ function SideNav() {
               <div className="sidenav-item">
                 <NavLink to="/admin" 
                   className={({ isActive }) => `${ isActive ? "active-sidenav" : "unactive-sidenav"}`}>
-                <UserCog id="icon" color="brown" size={20}></UserCog>
+                <UserCog className="icon" color="brown" size={20}></UserCog>
                 <span className="sidenav-word">Admin Page&nbsp;&nbsp;&nbsp;</span>
                 </NavLink>
               </div>
